c85b355291c3b0363a50604a61464a333ffa32bb
DeeprajDevikar <deeprajdevikar19@gmail.com>
2020-08-25 17:27:38 +0530

    post data from react to django with axios

--- img-upload-react/src/App.js
+++ img-upload-react/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
     constructor (props){
         super(props)
-        fetch(`http://127.0.0.1:8000/check/`,
-            {
-                mode: 'cors',
-                method: 'POST',
-                headers: {
-                    "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-                },
-                body: '&name=Deepraj',
+        const user = {
+                name: 'Deepraj',
             }
-        )
-        .then(res => res.json())
-        .then(json => {
-            console.log(json)
+        axios.post(`http://127.0.0.1:8000/check/`, { user })
+        .then(res => {
+            console.log(res)
         });
     }
     render() {
